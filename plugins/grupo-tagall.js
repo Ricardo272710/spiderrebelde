cfd17df95a692b51d64c6d39d402361fe218d6a2
Ricardo272710 <168157929+Ricardo272710@users.noreply.github.com>
2024-07-08 18:54:03 -0500

    Update grupo-tagall.js

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -1,25 +1,27 @@
-let handler = async(m, { isOwner, isAdmin, conn, text, participants, args, command }) => {
-if (!(isAdmin || isOwner)) {
-global.dfail('admin', m, conn)
-throw false
-}
-let pesan = args.join` `
-let oi = `*𝙈𝙚𝙣𝙨𝙖𝙟𝙚:* ${pesan}`
-let teks = `*⺀ ＡＣＴＩＶＥ ＧＲＵＰＯ 🗣️⺀*
+const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, command}) => {
+  if (!(isAdmin || isOwner)) {
+    global.dfail('admin', m, conn);
+    throw false;
+    var sum = member.length;
+  } else {
+    var sum = 0;
+    const total = 0;
+    var member = 0;
+  }
+  const pesan = args.join``;
+  const oi = `${pesan}`;
+  let teks = `╭─────────\n│❏ 𝙎𝙋𝙄𝘿𝙀𝙍 𝘽𝙊𝙏 \n│❏ 𝗠𝗶𝗲𝗺𝗯𝗿𝗼𝘀 : *${participants.length}* ${oi}\n│🥷🏻 𝘼𝘾𝙏𝙄𝙑𝘼𝙏𝙀 𝙈𝘼𝙉𝘾@\n│\n`;
+  for (const mem of participants) {
+    teks += `│🥷🏻 @${mem.id.split('@')[0]}\n`;
+  }
+  teks += `│\n╰𝙎𝙋𝙄𝘿𝙀𝙍 𝘽𝙊𝙏 `;
+  conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
+};
+handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
+handler.tags = ['group'];
+handler.command = /^(tagall|invocar|marcar|todos|invocación|ta)$/i;
+handler.admin = true;
+handler.group = true;
+export default handler;
 
-❏ ${oi}
 
-❏ *𝙀𝙩𝙞𝙦𝙪𝙚𝙩𝙖𝙨:*
-`
-for (let mem of participants) {
-teks += `➥ @${mem.id.split('@')[0]}\n`}
-teks += `➥ ${wm}`
-conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
-}
-handler.help = ['tagall <mesaje>','invocar <mesaje>']
-handler.tags = ['group']
-handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i
-handler.admin = true
-handler.group = true
-handler.botAdmin = true
-export default handler
